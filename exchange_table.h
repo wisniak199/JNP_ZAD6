ce1c298c52677b32154960339d947c173dbcd90d
Mateusz Piotrowski <mpp302@gmail.com>
2016-01-23 17:19:05 +0100

    lepszy exchange table

--- exchange_table.h
+++ exchange_table.h
@@ -7,16 +7,26 @@
 
 class ExchangeTable {
     private:
-        std::map<Currency, double> rates;
+        std::map<Currency, double> buyRates;
+        std::map<Currency, double> sellRates;
     public:
         ExchangeTable() {
-            rates[Currency::ENC] = 1;
-            rates[Currency::BIC] = 1;
-            rates[Currency::DIL] = 1;
-            rates[Currency::LIT] = 1;
+            buyRates[Currency::ENC] = 1;
+            buyRates[Currency::BIC] = 1;
+            buyRates[Currency::DIL] = 1;
+            buyRates[Currency::LIT] = 1;
+            sellRates[Currency::ENC] = 1;
+            sellRates[Currency::BIC] = 1;
+            sellRates[Currency::DIL] = 1;
+            sellRates[Currency::LIT] = 1;
         }
-        double getExchange(Currency c) const { return rates.find(c)->second; }
-        void setExchange(Currency c, double newRate) { rates[c] = newRate; }
+
+
+        double getBuyRate(Currency c) const { return buyRates.find(c)->second; }
+        double getSellRate(Currency c) const { return sellRates.find(c)->second; }
+
+        void setBuyRate(Currency c, double newRate) { buyRates[c] = newRate; }
+        void setSellRate(Currency c, double newRate) { sellRates[c] = newRate; }
 };
 
 #endif /*__EXCHANGE_TABLE_H__*/
